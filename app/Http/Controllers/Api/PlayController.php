b3e3ae7831d949cd23564900b3d941b51ad267fb
HP 450 G7 <babaeransari21@gmail.com>
2025-04-02 23:42:14 +0500

    modify uplaod play api

--- app/Http/Controllers/Api/PlayController.php
+++ app/Http/Controllers/Api/PlayController.php
@@ -22,7 +22,7 @@ class PlayController extends Controller
     {
         $play =  new Play();
         $request->validate([
-            'zip_file' => 'required|file|mimes:zip',
+            'image' => 'required',
             'play_name' => 'required|string',
             'play_type' => 'required|integer',
             'zone_selection' => 'required|integer',
@@ -33,44 +33,28 @@ class PlayController extends Controller
             'pre_snap_motion' => 'required|integer',
             'play_action_fake' => 'required|integer',
         ]);
+        $play = new Play();
+        $play->play_name = $request->play_name;
+        $play->play_type = $request->play_type;
+        $play->zone_selection = $request->zone_selection;
+        $play->min_expected_yard = $request->min_expected_yard;
+        $play->max_expected_yard = $request->max_expected_yard;
+        $play->target_offensive = $request->target_offensive;
 
-        $zip = $request->file('zip_file');
-        $zipPath = $zip->storeAs('temp', $zip->getClientOriginalName());
+                if($request->hasFile('image'))
+                {
+                    $path =   uploadImage($request->image,'public');
+                    $play->video_path = $path;
+                }
 
-        $zipFilePath = storage_path('app/' . $zipPath);
-        $extractPath = storage_path('app/temp/' . Str::random(10));
-        $zipArchive = new ZipArchive;
-        if ($zipArchive->open($zipFilePath) === TRUE) {
-            $zipArchive->extractTo($extractPath);
-            $zipArchive->close();
-
-
-            // Find the video file (assuming it's MP4)
-            $videoFile = collect(scandir($extractPath))->first(fn($file) => Str::endsWith($file, ['.mp4', '.avi', '.mov']));
-
-            if ($videoFile) {
-                $videoPath = 'uploads/videos/' . $videoFile;
-                Storage::disk('public')->put($videoPath, file_get_contents($extractPath . '/' . $videoFile));
-                $play = new Play();
-                $play->play_name = $request->play_name;
-                $play->play_type = $request->play_type;
-                $play->zone_selection = $request->zone_selection;
-                $play->min_expected_yard = $request->min_expected_yard;
-                $play->max_expected_yard = $request->max_expected_yard;
-                $play->target_offensive = $request->target_offensive;
                 $play->opposing_defensive = $request->opposing_defensive;
                 $play->pre_snap_motion = $request->pre_snap_motion;
                 $play->play_action_fake = $request->play_action_fake;
                 $play->type = $request->type;
-                $play->video_path = $videoPath;
+
                 $play->perfer_down_selection =  $request->perfer_down_selection;
                 $play->save();
-            }
 
-            // Cleanup temp files
-            unlink($zipFilePath);
-            Storage::deleteDirectory('temp');
-        }
         return new BaseResponse(STATUS_CODE_OK, STATUS_CODE_OK, "Play Uploaded Successfully ", $play);
 
     }
