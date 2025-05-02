a02d27b497d7b52f7444d3bfb1ba9aec96821634
Daniel Khan <dk@danielkhan.co>
2025-05-02 14:04:38 -0400

    added seeder

---
+++ database/seeders/SubsCription.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class SubsCription extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('subscription_plans')->insert([
+            // CFL Entry
+            [
+               'title'=>'Beginner'
+            ],
+            [
+                'title'=>'Silver'
+            ],
+            [
+                'title'=>'Gold'
+            ],
+            [
+                'title'=>'Premium'
+            ]
+        ]);
+    }
+}
