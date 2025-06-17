0d961688082795463487307b3e636fcf5e60d2f2
arbaz6161 <suhailarbaz94@gmail.com>
2025-06-17 17:05:44 -0400

    play mode changes

--- app/Http/Controllers/Api/PlayGameModeController.php
+++ app/Http/Controllers/Api/PlayGameModeController.php
@@ -35,33 +35,56 @@ class PlayGameModeController extends Controller
 
     public function addPoints(Request $request)
     {
-        try {
+       
+             $data = $request->all();
 
-                $game =  PlayGameMode::find($request->game_id);
-                $game->save();
+    // Initialize an array to hold the logs for batch insertion
+    $logs = [];
 
-                $game_log= new PlayGameLog();
-                $game_log->game_id =  $request->game_id;
-                $game_log->sport_id=  $request->sport_id;
-                $game_log->league_id=  $request->league_id;
-                $game_log->player_id=  $request->player_id;
-                $game_log->my_team_id=  $request->my_team_id;
-                $game_log->oponent_team_id=  $request->oponent_team_id;
-                $game_log->quater=  $request->quater;
-                $game_log->downs=  $request->downs;
-                $game_log->current_position=  $request->current_position;
-                $game_log->target=  $request->target;
-                $game_log->my_points=  $request->my_points;
-                $game_log->oponent_points=  $request->oponent_points;
-                $game_log->time=  $request->time;
-                $game_log->type_of_log=  $request->type_of_log;
-                $game_log->save();
+    // Start a database transaction
+    DB::beginTransaction();
 
-                DB::commit();
-             return new BaseResponse(STATUS_CODE_OK, STATUS_CODE_OK, "Update Changes Added ", $game);
-        } catch (\Throwable $th) {
-             DB::rollBack();
-            return new BaseResponse(STATUS_CODE_BADREQUEST, STATUS_CODE_BADREQUEST, $th->getMessage());
+    try {
+        // Loop through the incoming data
+        foreach ($data as $value) {
+            // Update the game record
+            $game = PlayGameMode::find($value['game_id']);
+            $game->save();
+
+            // Prepare the log data
+            $logs[] = [
+                'game_id' => $value['game_id'],
+                'sport_id' => $value['sport_id'],
+                'league_id' => $value['league_id'],
+                'player_id' => $value['player_id'],
+                'my_team_id' => $value['my_team_id'],
+                'oponent_team_id' => $value['oponent_team_id'],
+                'quater' => $value['quater'],
+                'downs' => $value['downs'],
+                'current_position' => $value['current_position'],
+                'target' => $value['target'],
+                'my_points' => $value['my_points'],
+                'oponent_points' => $value['oponent_points'],
+                'time' => $value['time'],
+                'type_of_log' => $value['type_of_log'],
+                'created_at' => now(),
+                'updated_at' => now(),
+            ];
         }
+
+        // Insert all logs in a single query
+        PlayGameLog::insert($logs);
+
+        // Commit the transaction
+        DB::commit();
+
+        return new BaseResponse(STATUS_CODE_OK, STATUS_CODE_OK, "Update Changes Added", $game);
+    } catch (\Throwable $th) {
+        // Rollback the transaction in case of an error
+        DB::rollBack();
+
+        return new BaseResponse(STATUS_CODE_BADREQUEST, STATUS_CODE_BADREQUEST, $th->getMessage());
+    }
+
     }
 }
