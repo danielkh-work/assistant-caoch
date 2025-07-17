779a94b3f84b320ebfce107c1f314172393bee11
arbaz6161 <suhailarbaz94@gmail.com>
2025-07-17 17:03:19 -0400

    update method can use in the sport controller

--- app/Http/Controllers/Api/SportController.php
+++ app/Http/Controllers/Api/SportController.php
@@ -85,7 +85,42 @@ class SportController extends Controller
           return new BaseResponse(STATUS_CODE_BADREQUEST, STATUS_CODE_BADREQUEST, $th->getMessage());
         }
     }
+    public function leagueUpdate(Request $request)
+    {
+        DB::beginTransaction();
+        try {
 
+           $League =  League::find($request->id);
+           $League->sport_id=$request->sport_id;
+           $League->league_rule_id=$request->league_rule_id;
+           $League->number_of_team=$request->number_of_team;
+           $League->title=$request->title;
+           $League->number_of_downs=$request->number_of_downs;
+           $League->length_of_field=$request->length_of_field;
+           $League->number_of_timeouts=$request->number_of_timeouts;
+           $League->clock_time=$request->clock_time;
+           $League->number_of_quarters=$request->number_of_quarters;
+           $League->length_of_quarters=$request->length_of_quarters;
+           $League->stop_time_reason=$request->stop_time_reason;
+           $League->overtime_rules=$request->overtime_rules;
+           $League->number_of_players=$request->number_of_players;
+           $League->flag_tbd =$request->flag_tbd;
+           $League->save();
+           LeagueTeam::where('league_id',$League->id)->delete();
+           foreach($request->team_name as $value)
+           {
+             $team =  new LeagueTeam;
+             $team->league_id =  $League->id;
+             $team->team_name = $value;
+             $team->save();
+           }
+           DB::commit();
+           return new BaseResponse(STATUS_CODE_OK, STATUS_CODE_OK, "League Updated  SuccessFully ", $League);
+        } catch (\Throwable $th) {
+          DB::rollBack();
+          return new BaseResponse(STATUS_CODE_BADREQUEST, STATUS_CODE_BADREQUEST, $th->getMessage());
+        }
+    }
     public function leagueView(Request $request)
     {
       $leauqe = League::with('teams','league_rule','sport')->find($request->id);
