ef51eaffcabc322f7fce5581e6ee012015f9f013
arbaz6161 <suhailarbaz94@gmail.com>
2026-01-19 05:59:05 -0800

    push work

---
+++ database/migrations/2026_01_16_192727_add_practice_number_players_to_leagues_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('leagues', function (Blueprint $table) {
+           $table->unsignedInteger('practice_number_players')
+            ->default(7)
+            ->after('number_of_players');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('leagues', function (Blueprint $table) {
+            $table->dropColumn('practice_number_players');
+        });
+    }
+};
