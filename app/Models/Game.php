e35eabc1bcd64d914bd6c910bc0419b82eff0f30
Arham1243 <arham404khan@gmail.com>
2026-07-24 20:07:37 +0500

    fix game fiillables

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -22,6 +22,10 @@ class Game extends Model
         'status',
         'match_start_date',
         'match_end_date',
+        'date',
+        'location',
+        'neutral_location',
+        'location_type',
     ];
 
     protected $casts = [
