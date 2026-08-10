423ab0c28f7bd5e67ebcf2ad5a53845eabc1d51f
Arham1243 <arham404khan@gmail.com>
2026-08-10 21:38:13 +0500

    feat: add SoftDeletes trait to multiple player-related models for improved data management

--- app/Models/ConfiguredPlayingTeamPlayer.php
+++ app/Models/ConfiguredPlayingTeamPlayer.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ConfiguredPlayingTeamPlayer extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
     protected $fillable= ['team_id','match_id','player_id','type','team_type','practice_player_id','game_type'];
     public function player()
     {
--- app/Models/TeamPlayerPosition.php
+++ app/Models/TeamPlayerPosition.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class TeamPlayerPosition extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
     protected $fillable = [
         'teamplayer_id',
         'position_name',
